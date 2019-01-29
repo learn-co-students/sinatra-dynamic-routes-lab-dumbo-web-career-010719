2ac0a01255edfb83117e486e12d9a0cc5b65a16b
edwinlin <edwinlin@gmail.com>
2019-01-28 20:11:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -23,7 +23,19 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-  	# binding.pry
+
   	erb :"#{params[:operation]}"
+
+  	# case params[:operation]
+   #  when 'add'
+   #    answer = (number1 + number2).to_s
+   #  when 'subtract'
+   #    answer = (number1 - number2).to_s
+   #  when 'multiply'
+   #    answer = (number1 * number2).to_s
+   #  when 'divide'
+   #    answer = (number1 / number2).to_s
+   #  end
+
   end
 end
